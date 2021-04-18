54fefa0fd6456b234396e2c7f1eb548826b1684c
César San Nicolás <cesarfsannicolasmartinez@gmail.com>
2021-04-18 20:28:33 +0200

    Removed unused method

--- test_tomcat.py
+++ test_tomcat.py
@@ -45,7 +45,13 @@ def get_response_content_and_code(url):
 
 def test_tomcat():
     docker_client = docker.from_env()
-    container = docker_client.containers.get('apache-tomcat-sample-container')
+    containers = docker_client.containers()
+    container = None
+    for cont in containers:
+        if '/apache-tomcat-sample-container' in cont.Names:
+            container = cont
+            break
+
     content, code = get_response_content_and_code('http://localhost:8080/sample')
     assert container is not None
     assert code == 200
